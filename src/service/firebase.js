a7d2dd86eb2d657b815cb43f3b46676dcdea37c5
Moonsunny <62421526+MoonSsunny@users.noreply.github.com>
2021-05-30 19:25:55 +0900

    add firebase

---
+++ src/service/firebase.js
@@ -0,0 +1,9 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKhqKqUYaDmxe6VJaOnmn1L9fQWkD_N9c",
+  authDomain: "business-card-maker-802a1.firebaseapp.com",
+  projectId: "business-card-maker-802a1",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
